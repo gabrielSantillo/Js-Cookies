13bc6d56d3e9a4cc27c840e62859688091ec6ed5
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-19 20:01:10 -0600

    updating

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@ let username = Cookies.get(`nothing`);
 if (typeof username === `string`) {
     document.querySelector(`button`).insertAdjacentHTML(`afterend`, `<h1>Congratulations for baking a cookie.</h1>`);
 }
+
+function delete_cookie(details) {
+    Cookies.remove(`button_clicked`);
+}
+
+let delete_button = document.getElementById(`delete_button`);
+delete_button.addEventListener(`click`, delete_cookie);
+
+
+function change_cookie(details) {
+    Cookies.set(`button_clicked`, `no`);
+}
+
+let change_button = document.getElementById(`for_change`);
+change_button.addEventListener(`click`, change_cookie);
